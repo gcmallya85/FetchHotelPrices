ef0cece887ffb60a48b486411dc4879fda39de05
gcmallya85 <gcmallya85@gmail.com>
2018-03-12 12:31:02 -0400

    Add code to read HTML from a user provided Tripadvisor URL (hardcoded in the code), use Beautiful Soup to traverse the content and read relevant information about hotel listings in the URL. Finally write the results to a csv file. Future work - create tables and plots?

--- Hotel_Pricing.py
+++ Hotel_Pricing.py
@@ -12,17 +12,41 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import random
+###### USER INPUT- Provide the Tripadvisor URL #####
+url = 'https://www.tripadvisor.com/Hotels-g37242-Lafayette_Indiana-Hotels.html' 
+######################
 
 with open('rates.csv', 'w') as rates:
-    rates.write("Date,Rate\n")
-    for m in range(3,4): #range(8,13):
-        for d in range(11,32):
-            hotel_price = random.randint(1,101)
-            # write dummy input into rates.csv
-            rates.write("2018-" + str(m) + "-" + str(d) + "," + str(hotel_price) + "\n")
-            
-#            web_response = requests.get('https://www.tripadvisor.com/Hotels-g37242-Lafayette_Indiana-Hotels.html')
-#            if response.ok:
-#                soup = BeautifulSoup(web_response.text)
+    # Read HTML content from the url provided by the user
+    web_response = requests.get(url)
+    
+    # Process the content only if the web request was successful
+    if web_response.ok:
+        # Write the header line to rates.csv file
+        rates.write("S.No.; Property Name; Best Price; Booking Website; User Rating; No. of Reviews\n")
+        
+        # Parse the HTML content using Beautiful Soup        
+        soup  = BeautifulSoup(web_response.text,"lxml")
+        
+        # Read all hotel listings available at the current url
+        hotels = soup.findAll('div','listing')
+        
+        # Loop through each listing to extract relevant details
+        ind = 0
+        for element in hotels:
+            if element.find('a','property_title'):
+                ind = ind + 1
+                hotel_name = element.find('a','property_title').get_text()
+                hotel_rating = element.find('span','ui_bubble_rating')['alt'].replace('bubbles','').strip()
+                hotel_review = element.find('a','review_count').get_text().replace('reviews','').strip()
+                if element.find('div','price'):
+                    hotel_price = element.find('div','price').get_text()
+                    booking_website = element.find('span','provider').get_text()
+#                    print '%d, %s, %s, %s, %s, %s' %(ind, hotel_name, hotel_price, booking_website,hotel_rating, hotel_review)
+                    rates.write(str(ind) + "; " + hotel_name + "; " + hotel_price + "; " + booking_website + "; " + hotel_rating + "; " + hotel_review + "\n")
+                else:
+#                    print '%d, %s, %s, %s, %s, %s' %(ind, hotel_name, 'NA', 'NA',hotel_rating, hotel_review)
+                    rates.write(str(ind) + "; " + hotel_name + "; " + "NA" + "; " + "NA" + "; " + hotel_rating + "; " + hotel_review + "\n")
+        
 #            # Do not abuse the internet!
 #            # time.sleep(1)
